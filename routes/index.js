e22e5ad9016c90214266a4c4de47323aac2b3a44
edrumm <esd0658@googlemail.com>
2021-01-17 17:08:01 +0000

    Testing new branch

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const Router = express.Router();
+
+// TODO: set up routes
+Router.get('/', (req, res) => {
+
+  // ...
+
+});
+
+Router.post('/', (req, res) => {
+
+  // ...
+
+});
+
+module.exports = Router;
