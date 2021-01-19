ff4ea6c04ae5cd9a29f8f00a7b8e1a512dc090ee
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-01-19 21:26:02 +0000

    created a navbar

---
+++ FrontEnd/picturethat/src/components/Navbar/MenuItems.js
@@ -0,0 +1,24 @@
+export const MenuItems = [
+    {
+        title: 'home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: '',
+        url: '',
+        cName: 'fas fa-user-circle'
+    },
+    
+]
+
