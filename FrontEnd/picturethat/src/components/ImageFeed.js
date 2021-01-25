6c466dafaa076a35dfbb36c083daa6708023e12b
ef27 <ef27@hw.ac.uk>
2021-01-25 17:50:11 +0000

    Updating Edit Profile Page

---
+++ FrontEnd/picturethat/src/components/ImageFeed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImageFeed = () => {
+
+    return (
+        <div className= "imageFeed">
+            images
+        </div>
+    )
+}
+
+export default ImageFeed;
+
+//https://www.youtube.com/watch?v=vUe91uOx7R0
