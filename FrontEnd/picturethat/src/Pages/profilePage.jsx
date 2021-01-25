936884dc655630ec7e6a7c01bcb9d989d35977d1
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-01-25 10:17:57 +0000

    Create profilePage.jsx

---
+++ FrontEnd/picturethat/src/Pages/profilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+
+const profilePage  = () => {
+    return (
+    
+    <Navbar></Navbar>
+    );
+};
+
+export default profilePage;
