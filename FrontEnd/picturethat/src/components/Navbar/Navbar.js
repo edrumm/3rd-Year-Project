5448d58483b2024a7a5dad84c150cb056d64099a
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-01-27 22:25:31 +0000

    css fixes

--- FrontEnd/picturethat/src/components/Navbar/Navbar.js
+++ FrontEnd/picturethat/src/components/Navbar/Navbar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Dropdown from './Dropdown';
 
+
+
     function Navbar() {
         const [clicked, setClicked] = useState(false);
 
@@ -20,16 +22,16 @@ import Dropdown from './Dropdown';
                 <h1 className="navbar-logo">PictureThat</h1>
                 <ul className="nav-menu">
                     <li>
-                        <Link to='/PictureThat' className='fas fa-home'/>
+                        <Link to='/PictureThat' className='fas fa-home a'/>
                     </li>
                     <li>
-                        <Link to='/' className='fas fa-search'/>
+                        <Link to='/' className='fas fa-search a'/>
                     </li>
                     <li>
-                        <Link to='/PictureThat/UploadPage' className='fas fa-images'/>
+                        <Link to='/PictureThat/UploadPage' className='fas fa-images a'/>
                     </li>
                     <li onClick={handleClick} >
-                        <i className='fas fa-user-circle top'/>
+                        <i className='fas fa-user-circle'/>
                         {clicked && <Dropdown />}
                     </li>
                 </ul>
@@ -37,19 +39,15 @@ import Dropdown from './Dropdown';
 
             <nav className="Navbarbottom">
             <ul className="nav-menubottom">
-                    <li>
-                        <Link to='/PictureThat' className='fas fa-home'/>
+            <li>
+                        <Link to='/PictureThat' className='fas fa-home b'/>
                     </li>
                     <li>
-                        <Link to='/' className='fas fa-search'/>
+                        <Link to='/' className='fas fa-search b'/>
                     </li>
                     <li>
-                        <Link to='/' className='fas fa-images'/>
+                        <Link to='/PictureThat/UploadPage' className='fas fa-images b'/>
                     </li>
-                    <li onClick={handleClick} >
-                        <i className='fas fa-user-circle top'/>
-                        {clicked && <Dropdown />}
-                    </li>   
             </ul>
 
             </nav>
