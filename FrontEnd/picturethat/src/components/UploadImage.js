30ce0a53203627c0cdfae969fdf595e3d1729b55
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-01-28 17:20:41 +0000

    Update UploadImage.js

--- FrontEnd/picturethat/src/components/UploadImage.js
+++ FrontEnd/picturethat/src/components/UploadImage.js
@@ -7,11 +7,21 @@ const ImageUpload = () => {
 
     const [image, setImage] = useState(null);
     const [url, setUrl] = useState("");
+    const [error, setError] = useState(null);
 
-    const handleChange = e => {
-        if (e.target.files[0]){
-            setImage(e.target.files[0]);
+    const imgTypes = ['image/png', 'image/jpeg'];
+
+    const imgChange = (e) => {
+        let selectedImg = e.target.files[0];
+        // checks to see if file has been selected and if its the correct type
+        if (selectedImg && imgTypes.includes(selectedImg.type)){
+            setImage(selectedImg);
+            setError('');
+        } else {
+            setImage(null);
+            setError('Incorrect Image type, please select a PNG or JPEG image');
         }
+        
     };
 
     const handleUpload = () => {
@@ -42,9 +52,10 @@ const ImageUpload = () => {
         <>
         <div className="container">
         <img src={url || "https://via.placeholder.com/400x380.png?text=Upload+Image"} alt="" className="images" />
-        <input accept="image/jpeg,image/png" type="file" onChange={handleChange} />
+        { error && <div className="error">{error}</div>}
+        <input type="file" onChange={imgChange} />
         <div>
-        <lable className="text" >Details</lable>
+        <a className="text" >Details</a>
         <input type="text" className="inputboxT" placeholder="Title" />
         <input type="text" className="inputboxD" placeholder="Description" />
         <input type="text" className="inputboxT" placeholder="Channel" />
