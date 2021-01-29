b6ffc2d15af45f2e336a9cfa728d1623b44788a4
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-01-29 23:23:18 +0000

    channels css

--- FrontEnd/picturethat/src/Pages/ChannelsPage.jsx
+++ FrontEnd/picturethat/src/Pages/ChannelsPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
+import './Channels.css';
 
 const AchievementsPage  = () => {
 
     return (
         <>
         <Navbar></Navbar>
+
+        <div>
+
+        </div>
+
         </>
     );
 };
