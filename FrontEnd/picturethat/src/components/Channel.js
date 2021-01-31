c0009ac474e24cf2d08f49089c8fe26335fd2e75
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-01-31 23:53:05 +0000

    added few things

--- FrontEnd/picturethat/src/components/Channel.js
+++ FrontEnd/picturethat/src/components/Channel.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './Channel.css';
 import dog from '../components/ImageFiles/jamie-street-uNNCs5kL70Q-unsplash.jpg';
+import cat from '../components/ImageFiles/iz-phil-pdALzg0yN-8-unsplash.jpg';
+import nature from '../components/ImageFiles/daniel-diesenreither-UIGiXbfQyIg-unsplash.jpg';
+import bird from '../components/ImageFiles/bechir-kaddech-6RgFFPEHbLY-unsplash.jpg';
 
 
 
@@ -10,6 +13,7 @@ import dog from '../components/ImageFiles/jamie-street-uNNCs5kL70Q-unsplash.jpg'
             <>
             <div className="Channel">
 
+                <div>
                 <div className="column channelPhoto">
                     <img src={dog} alt="" className="Channelimg"/>
                 </div>
@@ -17,12 +21,62 @@ import dog from '../components/ImageFiles/jamie-street-uNNCs5kL70Q-unsplash.jpg'
                 <div className="column channelInfo">
                     <label className="label">Channel Name: Dogs</label>
                     <br/>
+                    <label className="label">No of Followers: 1000 </label>
+                    <br/>
+                    <label className="label">No of Pictures: 20</label>
+                    <br/>
+                    <button className="button">See Channel</button>
+                </div>
+                </div>
+
+                <div className="card">
+                <div className="column channelPhoto">
+                    <img src={cat} alt="" className="Channelimg"/>
+                </div>
+                
+                <div className="column channelInfo">
+                    <label className="label">Channel Name: Cats</label>
+                    <br/>
                     <label className="label">No of Followers: 100 </label>
                     <br/>
                     <label className="label">No of Pictures: 1</label>
                     <br/>
                     <button className="button">See Channel</button>
                 </div>
+                </div>
+
+                <div>
+                <div className="column channelPhoto">
+                    <img src={nature} alt="" className="Channelimg"/>
+                </div>
+                
+                <div className="column channelInfo">
+                    <label className="label">Channel Name: Nature</label>
+                    <br/>
+                    <label className="label">No of Followers: 9500 </label>
+                    <br/>
+                    <label className="label">No of Pictures: 500</label>
+                    <br/>
+                    <button className="button">See Channel</button>
+                </div>
+                </div>
+
+                <div className="card">
+                <div className="column channelPhoto">
+                    <img src={bird} alt="" className="Channelimg"/>
+                </div>
+                
+                <div className="column channelInfo">
+                    <label className="label">Channel Name: Birds</label>
+                    <br/>
+                    <label className="label">No of Followers: 500 </label>
+                    <br/>
+                    <label className="label">No of Pictures: 25</label>
+                    <br/>
+                    <button className="button">See Channel</button>
+                </div>
+                </div>
+                
 
             </div>
             
