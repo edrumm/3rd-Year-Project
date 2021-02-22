0eeec847f4b2bc071fa21daef86ec46cf7b6a515
HWJB113 <jb113@hw.ac.uk>
2021-02-22 14:12:47 +0000

    Bringing frontEnd upload to backEnd

--- scripts/storage.js
+++ scripts/storage.js
@@ -18,8 +18,32 @@ const firebase = require('./firebase');
   fs for example, OR, should it be read client (React) side and just take an image
   as the img parameter??
 */
-module.exports.upload = /* async */ (db, bucket, img) => {
 
+
+/*  jake note: Currently modifying Lucas' upload code, tweaking values and will update frontEnd to have similar fetch
+    as login/signup. */
+module.exports.upload = /* async */ (data, bucket, img) => {
+  const uploadTask = firebase.ref(`images/${img.name}`).put(img);
+  const collection = firebase.collection('posts');
+  uploadTask.on(
+      "state_changed",
+      snapshot => {},
+      error => {
+          console.log(error);
+      },
+      () => {
+          storage
+              .ref("images")
+              .child(img.name)
+              .getDownloadURL()
+              .then(url => { 
+                  console.log(url);
+                  //return the url as part of data returned
+                  const uploaddate = timestamp();
+                  collection.add({ url: url, uploaddate, data.title, data.loc, data.description});
+              });
+      }     
+      
 };
 
 module.exports.download = /* async */ (db, bucket, url) => {
