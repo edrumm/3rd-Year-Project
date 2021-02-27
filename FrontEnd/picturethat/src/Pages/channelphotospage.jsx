ea738c3cdb97a6aa411c54773fb4a13300a2840e
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-02-27 21:07:10 +0000

    Update channelphotospage.jsx

--- FrontEnd/picturethat/src/Pages/channelphotospage.jsx
+++ FrontEnd/picturethat/src/Pages/channelphotospage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
+import ImageFeed from '../components/ImageFeed';
 
 const ChannelPhotos = () => {
 
     return (
         <>
         <Navbar></Navbar>
-
+        <ImageFeed />
         </>
     );
 };
