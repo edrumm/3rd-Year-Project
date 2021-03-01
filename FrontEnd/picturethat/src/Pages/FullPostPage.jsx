d2290a6d5f4faadf579b38f27cd328eac6b1e3d4
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-03-01 19:08:59 +0000

    added new page

---
+++ FrontEnd/picturethat/src/Pages/FullPostPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import FullPost from '../components/FullPost';
+
+const LandingPage  = () => {
+    return (
+    <>
+    <Navbar />
+    <FullPost />
+    </>
+    );
+};
+
+export default LandingPage;
---
+++ FrontEnd/picturethat/src/components/FullPost.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+
+
+import './FullPost.css';
+
+
+    function FullPost() {
+       
+        return(
+            <>  
+
+
+            </>
+        )
+    }
+
+
+export default FullPost;
+
