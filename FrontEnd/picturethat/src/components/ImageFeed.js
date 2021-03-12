cf33764d8fd62c4e4016ccf7140a6942d1883922
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2021-03-12 15:05:28 +0000

    Tiny fix

--- FrontEnd/picturethat/src/components/ImageFeed.js
+++ FrontEnd/picturethat/src/components/ImageFeed.js
@@ -3,7 +3,7 @@ import dog from '../components/ImageFiles/iz-phil-pdALzg0yN-8-unsplash.jpg';
 import './ImageFeed.css';
 //import getImg from '../getImg';
 import {Link} from 'react-router-dom';
-import firebase from "../firebase.js";
+import firebase from "../firebase";
 //import PopUp from '../components/PostPopUp';
 
 let setSelectedImgId;
@@ -13,14 +13,14 @@ const ImageFeed = () => {
     const { docs } = firebase.GetImg('posts');
     console.log(docs);
 
-    
-    
+
+
     // const setSelectedImgId = (id) => {
     //     imgId = id;
     //     console.log(imgId);
     // }
-    
-    
+
+
     //return selectedImgId;
 
     // const [showPopUp, setShowPopUp] = useState(false);
@@ -28,9 +28,9 @@ const ImageFeed = () => {
     // const openPopUp = () => {
     //     setShowPopUp(prev => !prev)
     // };
-      
+
     return (
-      
+
         <div className= "imageFeed">
             { docs && docs.map(doc => (
                 <div class="post" key={doc.id}>
@@ -54,7 +54,7 @@ const ImageFeed = () => {
                     <div>
                         <img src={doc.url} alt="" className="image"/>
                     </div>
-                    
+
                     <div className="bottominfo">
                     <div className="postDetailsContainer">
                     <div className="buttonfield">
@@ -71,13 +71,13 @@ const ImageFeed = () => {
                         <label>Date: </label>
                     </div>
                 </div>
-                
+
             </div>
         </div>
         ))}
     </div>
     )
-    
+
 }
 
 export default ImageFeed ;
@@ -86,7 +86,7 @@ export {setSelectedImgId};
 
 
 //the source bellow was used to help set up how to send and get data from database
-//https://www.youtube.com/watch?v=vUe91uOx7R0&ab_channel=TraversyMedia 
+//https://www.youtube.com/watch?v=vUe91uOx7R0&ab_channel=TraversyMedia
 //https://www.youtube.com/watch?v=vUe91uOx7R0
 
 //<input type="text" className="commentinput" placeholder="Add a comment"></input>
