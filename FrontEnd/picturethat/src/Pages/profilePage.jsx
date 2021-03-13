9060e35c666681e45b336c415ff8bf0d7108d6b4
ef27 <47322206+eniolubifaleye@users.noreply.github.com>
2021-03-13 19:11:56 +0000

    removed the imported pictures

--- FrontEnd/picturethat/src/Pages/profilePage.jsx
+++ FrontEnd/picturethat/src/Pages/profilePage.jsx
@@ -8,12 +8,6 @@ import ImageGrid from '../components/ImageGrid';
 
 /* import for image files DEMO */
 import dog from '../components/ImageFiles/iz-phil-pdALzg0yN-8-unsplash.jpg';
-import pic1 from '../components/ImageFiles/picture1.jpg';
-import pic2 from '../components/ImageFiles/picture2.jpg';
-import pic3 from '../components/ImageFiles/picture3.jpg';
-import pic4 from '../components/ImageFiles/picture4.jpg';
-import pic5 from '../components/ImageFiles/picture5.jpg';
-import pic6 from '../components/ImageFiles/picture6.jpg';
 import Footer from '../components/footer';
 
 import firebase from '../firebase';
