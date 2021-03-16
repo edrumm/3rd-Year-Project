51ed931d72d3e74fe45b7b787806abd546398c4b
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-03-16 11:13:14 +0000

    Update firebase.js

--- FrontEnd/picturethat/src/firebase.js
+++ FrontEnd/picturethat/src/firebase.js
@@ -88,12 +88,12 @@ const logout = () => {
 };
 
 const ResetEmail = (email) => {
-  var auth = firebase.auth();
   var emailAddress = email;
 
   auth.sendPasswordResetEmail(emailAddress).then(function() {
     // Email sent.
     }).catch(function(error) {
+      console.log(error);
     // An error happened.
     });
 };
