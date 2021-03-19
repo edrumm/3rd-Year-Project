2e07a2824007061bd40bd4b0b514c1b1aff05a34
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-03-19 09:54:03 +0000

    comments working

--- FrontEnd/picturethat/src/firebase.js
+++ FrontEnd/picturethat/src/firebase.js
@@ -272,12 +272,12 @@ const UploadPost = async (caption, loc, channel, image, username) => {
 const AddComment = async (username, text, post) => {
 
   const refcom = firestore.collection('comments').doc();
-  let postref = firestore.collection('posts').doc(post);
+  //let postref = firestore.collection('posts').doc(post);
   const Data = {
     uploaddate: firebase.firestore.Timestamp.now().toDate(),
     username: username,
     text: text,
-    post: postref
+    post: post
   }
   await refcom.set(Data);
 
@@ -298,11 +298,11 @@ const AddComment = async (username, text, post) => {
 
 const GetComments = (postid) => {
   const [docs, setDocs] = useState([]);
-  let postref = "/posts/" + postid;
+  let postref = postid;
   
   useEffect(() => {
       const unsub = firestore.collection('comments')
-          //.where('post', '==', postref)
+          .where('post', '==', postref)
           .orderBy('uploaddate', 'desc')
           .onSnapshot((snap) => {
               let documents = [];
