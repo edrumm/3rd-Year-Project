46ea5a20c6faa299dca86750b1902827418c3ac5
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-03-22 18:57:25 +0000

    made few changes

--- FrontEnd/picturethat/src/Pages/ProfilePage.jsx
+++ FrontEnd/picturethat/src/Pages/ProfilePage.jsx
@@ -7,13 +7,7 @@ import {useState} from 'react';
 import ImageGrid from '../components/ImageGrid';
 
 /* import for image files DEMO */
-import dog from '../components/ImageFiles/iz-phil-pdALzg0yN-8-unsplash.jpg';
-import pic1 from '../components/ImageFiles/picture1.jpg';
-import pic2 from '../components/ImageFiles/picture2.jpg';
-import pic3 from '../components/ImageFiles/picture3.jpg';
-import pic4 from '../components/ImageFiles/picture4.jpg';
-import pic5 from '../components/ImageFiles/picture5.jpg';
-import pic6 from '../components/ImageFiles/picture6.jpg';
+
 import Footer from '../components/footer';
 
 import firebase from '../firebase';
@@ -62,7 +56,7 @@ const ProfilePage  = () => {
                 <p>{dataDocs.username}</p>
             ))}
             
-            <img src={dog} alt="" className="profileimageProfile" />
+            <img src="" alt="" className="profileimageProfile" />
 
             {isEdit ? renderEdit() : renderDefault()}
         </div>
