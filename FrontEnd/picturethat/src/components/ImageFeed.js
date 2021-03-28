b927edac8655f5b2e90035c3a0d433bec71151fc
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2021-03-28 23:05:19 +0100

    Update ImageFeed.js

--- FrontEnd/picturethat/src/components/ImageFeed.js
+++ FrontEnd/picturethat/src/components/ImageFeed.js
@@ -31,6 +31,10 @@ const ImageFeed = () => {
                     </div>
                     </div>
 
+                    <div>
+                        <img src={doc.url} alt="" className="image"/>
+                    </div>
+                    
                     <div className="bottominfo">
                     <div className="postDetailsContainer">
 
